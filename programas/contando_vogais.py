39c694abe95047c03b7fd139638bd2aa288e6d32
tamara349 <rodriguestamara005@gmail.com>
2026-03-03 22:31:19 -0300

    contador de vogais

---
+++ programas/contando_vogais.py
@@ -0,0 +1,13 @@
+def contar_vogais(texto):  
+    vogais = "aâãàáeéèêiíoóôõuú"
+    quantidade = 0  
+ 
+    for letra in texto.lower():  
+        if letra in vogais:  
+            quantidade += 1  
+ 
+    return quantidade  
+ 
+texto = input("Digite um texto: ") 
+ 
+print(f"O texto contém {contar_vogais(texto)} vogais.")
