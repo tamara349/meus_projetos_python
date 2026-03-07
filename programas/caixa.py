a6f0ab50855cecc1809a41fe1eeb5e9e78dc070a
tamara349 <rodriguestamara005@gmail.com>
2026-03-07 19:24:04 -0300

    Atualiza função de caixa eletrônico para incluir cédulas de R$ 200 e remove validação de múltiplo de 2

--- programas/caixa.py
+++ programas/caixa.py
@@ -1,21 +1,21 @@
 def caixa_eletronico(): 
-    cedulas = [100, 50, 20, 10, 5, 2] 
- 
+    # Lista de cédulas disponíveis
+    cedulas = [200,100, 50, 20, 10, 5, 2,1] 
+ # Solicita o valor do saque ao usuário
     try: 
         valor = int(input("Digite o valor do saque: ")) 
- 
+ # Verifica se o valor é positivo e múltiplo de 2
         if valor <= 0: 
             print("Erro: O valor deve ser positivo.")
-        elif valor % 2 != 0: 
-            print("Erro: O valor deve ser múltiplo de 2.")
+
         else: 
             print("Cédulas entregues:")
-            
+           # Calcula a quantidade de cada cédula necessária para o saque 
             for cedula in cedulas: 
                 quantidade = valor // cedula
                 if quantidade > 0:
                     print(f"{quantidade} cédulas de R$ {cedula}")
-                    valor = valor % cedula 
+                    valor = valor % cedula # Atualiza o valor restante para o próximo cálculo
  
     except ValueError: 
         print("Erro: Digite um valor numérico válido.") 
